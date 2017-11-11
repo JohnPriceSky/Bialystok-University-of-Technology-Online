4c60cae3a230955c63c2b045fa43849215200ece
Michał Sadowski <michallo.sadek@gmail.com>
2017-11-11 22:18:23 +0100

    added angular

--- BUOTOnline/BUOTOnline.Web/App_Start/BundleConfig.cs
+++ BUOTOnline/BUOTOnline.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,13 @@ namespace BUOTOnline.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/app/app.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/homeController").Include(
+                        "~/Scripts/app/controllers/homeController.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
